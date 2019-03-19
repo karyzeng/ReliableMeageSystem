3471cf36db84300a953acdc8cfa43761c568df95
karyzeng <354697171@qq.com>
2019-03-19 16:43:33 +0800

    activeMQ的demo

---
+++ zzp.activemq/src/main/java/com/zzp/activemq/common/Constants.java
@@ -0,0 +1,11 @@
+package com.zzp.activemq.common;
+
+public class Constants {
+
+    public static final String MQ_NAME = "user";
+
+    public static final String MQ_PASSWORD = "user";
+
+    public static final String MQ_BROKETURL = "tcp://127.0.0.1:61616";
+
+}
